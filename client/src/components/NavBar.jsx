1a79ea8255f46dcbd7b7fa57bc88a4f4951c7b4e
rinmatjam2127 <rinmatjam15@gmail.com>
2026-09-22 18:49:02 -0500

    logo added to NavBar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import { NavLink } from "react-router-dom";
 export function NavBar() {
     return (
         <nav className="flex items-center justify-between px-4 py-3 border-b border-gray-200">
+            <img src="/favicon.svg" alt="Inkwell logo" className="w-6 h-6" />
             <span className="font-bold text-lg">Inkwell</span>
             <div className="flex gap-4">
                 <NavLink
